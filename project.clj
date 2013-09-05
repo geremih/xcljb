8fabad3f914722ff41ea189600ed88a4a2f9516a
Vincent W. Chen <noodlewiz@gmail.com>
2013-09-04 17:37:15 -0700

    Update gloss dependency

--- project.clj
+++ project.clj
@@ -6,4 +6,4 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/data.xml "0.0.7"]
                  [org.slf4j/slf4j-log4j12 "1.7.5"]
-                 [gloss "0.2.2-beta4"]])
+                 [gloss "0.2.2"]])
