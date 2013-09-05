4bd8487ee1c0bf0ca196de8675537e64c345f6f4
Vincent W. Chen <noodlewiz@gmail.com>
2013-09-04 17:44:25 -0700

    Remove project license URL
    Remove project license URL, since it's different from the provided
    LICENSE and therefore misleading.

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
 (defproject xcljb "0.1.0-SNAPSHOT"
   :description "X protocol Clojure-language Binding"
   :url "https://github.com/noodlewiz/xcljb"
-  :license {:name "The MIT License (MIT)"
-            :url "http://opensource.org/licenses/MIT"}
+  :license {:name "The MIT License (MIT)"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/data.xml "0.0.7"]
                  [org.slf4j/slf4j-log4j12 "1.7.5"]
