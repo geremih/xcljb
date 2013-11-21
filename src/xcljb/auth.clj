17d3b19117df4a47585af7dccb59668ccfc79e78
Vincent W. Chen <noodlewiz@gmail.com>
2013-11-20 23:03:58 -0800

    Remove unneeded functions
    Reading and then decoding from channel is now only needed by auth, so
    moving the functions from common to auth.

--- src/xcljb/auth.clj
+++ src/xcljb/auth.clj
@@ -1,19 +1,36 @@
 (ns xcljb.auth
-  (:require [xcljb.common :as common]))
+  (:require [gloss core io])
+  (:import java.nio.ByteBuffer))
+
+(defn- read-bytes [ch bytes]
+  (let [buf (ByteBuffer/allocate bytes)]
+    (.read ch buf)
+    (.rewind buf)
+    (gloss.io/decode (case bytes
+                       1 :ubyte
+                       2 :uint16
+                       4 :uint32)
+                     buf)))
+
+(defn- read-string [ch length]
+  (let [buf (ByteBuffer/allocate length)]
+    (.read ch buf)
+    (.rewind buf)
+    (gloss.io/decode (gloss.core/string :ascii) buf)))
 
 (defn- parse-xauthority []
   (when-let [auth-file (System/getenv "XAUTHORITY")]
     (with-open [ins (clojure.java.io/input-stream auth-file)
                 ch (java.nio.channels.Channels/newChannel ins)]
-      (let [family (common/read-bytes ch 2)
-            addrlen (common/read-bytes ch 2)
-            addr (common/read-string ch addrlen)
-            numlen (common/read-bytes ch 2)
-            num (common/read-string ch numlen)
-            namelen (common/read-bytes ch 2)
-            name (common/read-string ch namelen)
-            datalen (common/read-bytes ch 2)
-            data (doall (repeatedly datalen #(common/read-bytes ch 1)))]
+      (let [family (read-bytes ch 2)
+            addrlen (read-bytes ch 2)
+            addr (read-string ch addrlen)
+            numlen (read-bytes ch 2)
+            num (read-string ch numlen)
+            namelen (read-bytes ch 2)
+            name (read-string ch namelen)
+            datalen (read-bytes ch 2)
+            data (doall (repeatedly datalen #(read-bytes ch 1)))]
         {:family family
          :addr addr
          :num num
--- src/xcljb/common.clj
+++ src/xcljb/common.clj
@@ -1,38 +1,10 @@
-(ns xcljb.common
-  (:use [gloss io])
-  (:require [gloss.core :as glcore])
-  (:import [java.nio ByteBuffer]))
+(ns xcljb.common)
 
 (defmulti read-reply (fn [ext-name opcode _] [ext-name opcode]))
 
 (defmulti read-event (fn [ext-name n _] [ext-name n]))
 
 (defmulti read-error (fn [ext-name n _] [ext-name n]))
-
-(def ^:private BUFFER (ByteBuffer/allocateDirect 2048))
-
-(defn read-bytes [ch bytes & {:keys [signed float]
-                              :or {signed false, float false}}]
-  (-> BUFFER (.position 0) (.limit bytes))
-  (.read ch BUFFER)
-  (.rewind BUFFER)
-  (cond
-   (and float (= bytes 4)) (decode :float32 BUFFER)
-   (and float (= bytes 8)) (decode :float64 BUFFER)
-   signed (case bytes
-            1 (decode :byte BUFFER)
-            2 (decode :int16 BUFFER)
-            4 (decode :int32 BUFFER))
-   :else (case bytes
-           1 (decode :ubyte BUFFER)
-           2 (decode :uint16 BUFFER)
-           4 (decode :uint32 BUFFER))))
-
-(defn read-string [ch length]
-  (-> BUFFER (.position 0) (.limit length))
-  (.read ch BUFFER)
-  (.rewind BUFFER)
-  (decode (glcore/string :ascii) BUFFER))
 
 (defn padding [n]
   (rem (- 4 (rem n 4))
