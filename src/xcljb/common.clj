77740b1f277a3215cfc0a5a15ffc1cb7ac06a4ff
Vincent W. Chen <noodlewiz@gmail.com>
2013-11-20 22:24:47 -0800

    Remove unneeded functions from common

--- src/xcljb/common.clj
+++ src/xcljb/common.clj
@@ -3,24 +3,11 @@
   (:require [gloss.core :as glcore])
   (:import [java.nio ByteBuffer]))
 
-(defprotocol Serializable
-  (to-frame [this])
-  (to-value [this]))
+(defmulti read-reply (fn [ext-name opcode _] [ext-name opcode]))
 
-(defprotocol Measurable
-  (sizeof [this]))
+(defmulti read-event (fn [ext-name n _] [ext-name n]))
 
-(defprotocol Request
-  (opcode [this]))
-
-(defprotocol ReadableType
-  (read-type [this ch]))
-
-(defmulti read-reply (fn [ext-name opcode & _] [ext-name opcode]))
-
-(defmulti read-event (fn [ext-name n & _] [ext-name n]))
-
-(defmulti read-error (fn [ext-name n & _] [ext-name n]))
+(defmulti read-error (fn [ext-name n _] [ext-name n]))
 
 (def ^:private BUFFER (ByteBuffer/allocateDirect 2048))
 
@@ -47,11 +34,6 @@
   (.rewind BUFFER)
   (decode (glcore/string :ascii) BUFFER))
 
-(defn read-pad [ch bytes]
-  (-> BUFFER (.position 0) (.limit bytes))
-  (.read ch BUFFER)
-  nil)
-
 (defn padding [n]
   (rem (- 4 (rem n 4))
        4))
@@ -59,39 +41,3 @@
 (defn bit-count [n]
   (.bitCount (BigInteger/valueOf n)))
 
-(defn valueparam->value [vp]
-  [(reduce bit-or 0 (keys vp))
-   (->> vp (sort-by key) (map second))])
-
-(defn mask->masks [mask]
-  (for [m (iterate #(bit-shift-left % 1) 1)
-        :while (<= m mask)
-        :when (not (zero? (bit-and mask m)))]
-    m))
-
-(defrecord PrimitiveType [type]
-  Measurable
-  (sizeof [this]
-    (case (:type this)
-      (:ubyte :byte) 1
-      (:uint16 :int16) 2
-      (:uint32 :int32 :float32) 4
-      :float64 8))
-
-  Serializable
-  (to-frame [this]
-    (:type this))
-  (to-value [_]
-    (throw (Exception. "Calling to-value on PrimitiveType.")))
-
-  ReadableType
-  (read-type [this ch]
-    (case (:type this)
-      :ubyte (read-bytes ch 1)
-      :uint16 (read-bytes ch 2)
-      :uint32 (read-bytes ch 4)
-      :byte (read-bytes ch 1 :signed true)
-      :int16 (read-bytes ch 2 :signed true)
-      :int32 (read-bytes ch 4 :signed true)
-      :float32 (read-bytes ch 4 :float true)
-      :float64 (read-bytes ch 8 :float true))))
