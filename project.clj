f2f7b2b82dc8133ea7cb2073f9742602d4751424
Mihir Rege <mihirrege@gmail.com>
2016-04-29 18:33:43 +0530

    Update dependencies

--- project.clj
+++ project.clj
@@ -2,8 +2,8 @@
   :description "X protocol Clojure-language Binding"
   :url "https://github.com/noodlewiz/xcljb"
   :license {:name "The MIT License (MIT)"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/data.xml "0.0.7"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/data.xml "0.0.8"]
                  [org.slf4j/slf4j-log4j12 "1.7.5"]
-                 [gloss "0.2.2"]]
+                 [gloss "0.2.6"]]
   :jvm-opts ["-server"])
